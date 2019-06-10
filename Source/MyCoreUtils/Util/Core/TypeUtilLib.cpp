acdab257860e0acb2bb2c31866770b8cb5525f4b
alexey_eng <alexey_eng@mail.ru>
2019-06-10 22:06:36 +0500

    Adds TypeUtilLib library

---
+++ Source/MyCoreUtils/Util/Core/TypeUtilLib.cpp
@@ -0,0 +1,22 @@
+#include "TypeUtilLib.h"
+#include "LogUtilLib.h"
+
+int32 UTypeUtilLib::FindEnumIndexByName(const FString& InEnumName, const FString& InString)
+{
+	UEnum* C = FindObject<UEnum>(ANY_PACKAGE, *InEnumName, /*ExactClass*/true);
+	if(C == nullptr)
+	{
+		return INDEX_NONE;
+	}
+	else
+	{
+		return C->GetIndexByName(FName(*InString, FNAME_Find));
+	}
+}
+
+int32 UTypeUtilLib::FindEnumIndexByNameChecked(const FString& InEnumName, const FString& InString)
+{
+	int32 const LiteralIndex = FindEnumIndexByName(InEnumName, InString);
+	checkf(LiteralIndex, TEXT("%s failed, maybe try unchecked version instead"), __FUNCTION__);
+	return LiteralIndex;
+}
---
+++ Source/MyCoreUtils/Util/Core/TypeUtilLib.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "Misc/Optional.h"
+#include "TypeUtilLib.generated.h"
+
+UCLASS()
+class UTypeUtilLib : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+	// ~Enums Begin
+	/**
+	* If enum with the given name or literal does not exist, otherwise returns negative, 
+	* otherwise returns enum's literal value.
+	*/
+	UFUNCTION(BlueprintPure, Category = TypeUtilLib)
+	static int32 FindEnumIndexByName(const FString& InEnumName, const FString& InString);
+
+	/**
+	* Returns enum literal value.
+	* @warning: the literal and enum value must exist.
+	* @see: FindEnumIndexByName
+	*/
+	UFUNCTION(BlueprintPure, Category = TypeUtilLib)
+	static int32 FindEnumIndexByNameChecked(const FString& InEnumName, const FString& InString);
+	// ~Enums End
+};
