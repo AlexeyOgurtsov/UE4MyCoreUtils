56c2fbbc82793d6905181547d403c072030a2106
alexey_eng <alexey_eng@mail.ru>
2019-06-11 12:03:15 +0500

    Fix Clean bat

--- MyCleanCoreUtils.bat
+++ MyCleanCoreUtils.bat
@@ -1,4 +1,4 @@
 D:
-set location=D:\UNR_SCRIPT\LIB\
-set sniploc=D:\UNR_SCRIPT\SNIPPETS\
+set location=D:\UNR_SCRIPT\LIB
+set sniploc=D:\UNR_SCRIPT\SNIPPETS
 call %sniploc%\MyClean.bat %location%\MyCoreUtils
