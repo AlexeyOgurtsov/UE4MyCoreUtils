c3c73a465db66c775f24e6a3d3e6fe1ef7d583cf
alexey_eng <alexey_eng@mail.ru>
2019-07-11 02:13:56 +0500

    Add TUVisibleActor (test util), not yet impl

---
+++ Source/MyCoreUtils/Util/TestUtil/TUVisibleActor.h
@@ -0,0 +1,54 @@
+#pragma once
+
+/**
+* Actor that has collision and mesh.
+*/
+
+#include "TUActor.h"
+#include "TUVisibleActor.generated.h"
+
+class UCameraComponent;
+class USpringArmComponent;
+class USceneComponent;
+class UStaticMeshComponent;
+class USphereComponent;
+
+UCLASS()
+class ATUVisibleActor : public ATUActor
+{
+	GENERATED_BODY()
+
+public:
+	ATUVisibleActor();
+
+	UFUNCTION(BlueprintPure, Category = Components)
+	USceneComponent* GetRootSceneComponent() const { return RootSceneComponent; }
+
+	UFUNCTION(BlueprintPure, Category = Camera)
+	UCameraComponent* GetCamera() const { return Camera; }
+
+	UFUNCTION(BlueprintPure, Category = Visual)
+	UStaticMeshComponent* GetMesh() const { return Mesh; }
+
+	UFUNCTION(BlueprintPure, Category = Collision)
+	USphereComponent* GetProxSphere() const { return ProxSphere; }
+
+private:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = Components, Meta=(AllowPrivateAccess = true))
+	USceneComponent* RootSceneComponent = nullptr;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = Camera, Meta=(AllowPrivateAccess = true))
+	UCameraComponent* Camera = nullptr;
+	void InitCameraAndSpringArm(USceneComponent* InAttachTo);
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = Camera, Meta=(AllowPrivateAccess = true))
+	USpringArmComponent* SpringArm = nullptr;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = Visual, Meta=(AllowPrivateAccess = true))
+	UStaticMeshComponent* Mesh = nullptr;
+	void InitMesh(USceneComponent* InAttachTo);
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = Collision, Meta=(AllowPrivateAccess = true))
+	USphereComponent* ProxSphere = nullptr;
+	void InitProxSphere(USceneComponent* InAttachTo);
+};
