fc6d4f57d926090b95ab72961f4643cd19225dbd
alexey_eng <alexey_eng@mail.ru>
2019-11-10 15:05:35 +0500

    Fix TUPawn collision

--- Source/MyCoreUtils/Util/TestUtil/TUPawn.cpp
+++ Source/MyCoreUtils/Util/TestUtil/TUPawn.cpp
@@ -15,15 +15,17 @@
 
 #include "UObject/ConstructorHelpers.h"
 
-using namespace VisibleActorConfig;
 
 ATUPawn::ATUPawn()
 {
-	RootComponent = RootSceneComponent = CreateDefaultSubobject<USceneComponent>(TEXT("RootSceneComponent"));
+	InitMesh(nullptr);
+	RootComponent = Mesh;
 
-	InitCameraAndSpringArm(RootSceneComponent);
-	InitMesh(RootSceneComponent);
+	RootSceneComponent = CreateDefaultSubobject<USceneComponent>(TEXT("RootSceneComponent"));
+	RootSceneComponent->SetupAttachment(RootComponent);
+
 	InitProxSphere(RootSceneComponent);
+	InitCameraAndSpringArm(RootSceneComponent);
 }
 
 void ATUPawn::BeginPlay()
@@ -52,48 +54,51 @@ void ATUPawn::BeginPlayFinished()
 
 void ATUPawn::InitCameraAndSpringArm(USceneComponent* InAttachTo)
 {
-	checkf(InAttachTo, TEXT("When calling %s component to attach to must be non-NULL pointer"), TEXT(__FUNCTION__));
-
 	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
-	SpringArm->TargetArmLength = Default::SPRINGARM_TARGET_ARM_LENGTH;
-	SpringArm->RelativeRotation = Default::SPRINGARM_RELATIVE_ROTATION;
-	SpringArm->RelativeLocation = Default::SPRINGARM_RELATIVE_LOCATION;
-	SpringArm->bEnableCameraLag = Default::SPRINGARM_ENABLE_CAMERA_LAG;
-	SpringArm->CameraLagSpeed = Default::SPRINGARM_CAMERA_LAG_SPEED;
+	SpringArm->TargetArmLength = VisibleActorConfig::Default::SPRINGARM_TARGET_ARM_LENGTH;
+	SpringArm->RelativeRotation = VisibleActorConfig::Default::SPRINGARM_RELATIVE_ROTATION;
+	SpringArm->RelativeLocation = VisibleActorConfig::Default::SPRINGARM_RELATIVE_LOCATION;
+	SpringArm->bEnableCameraLag = VisibleActorConfig::Default::SPRINGARM_ENABLE_CAMERA_LAG;
+	SpringArm->CameraLagSpeed = VisibleActorConfig::Default::SPRINGARM_CAMERA_LAG_SPEED;
 	SpringArm->SetupAttachment(InAttachTo);
 
 	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
 	Camera->bUsePawnControlRotation = true;
-	Camera->SetupAttachment(SpringArm, USpringArmComponent::SocketName);
+	if(InAttachTo)
+	{
+		Camera->SetupAttachment(SpringArm, USpringArmComponent::SocketName);
+	}
 }
 
 void ATUPawn::InitMesh(USceneComponent* InAttachTo)
 {
-	checkf(InAttachTo, TEXT("When calling %s component to attach to must be non-NULL pointer"), TEXT(__FUNCTION__));
-
-	static ConstructorHelpers::FObjectFinderOptional<UStaticMesh> MeshFinder { Default::MESH_ASSET_PATH };
-	M_LOG_ERROR_IF( ! MeshFinder.Succeeded(), TEXT("Default mesh (\"%s\") NOT found"), Default::MESH_ASSET_PATH);
+	static ConstructorHelpers::FObjectFinderOptional<UStaticMesh> MeshFinder { VisibleActorConfig::Default::MESH_ASSET_PATH };
+	M_LOG_ERROR_IF( ! MeshFinder.Succeeded(), TEXT("Default mesh (\"%s\") NOT found"), VisibleActorConfig::Default::MESH_ASSET_PATH);
 
 	{
 		Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
 		if(MeshFinder.Succeeded())
 		{
-			M_LOG(TEXT("Default mesh (\"%s\") found, setting it up"), Default::MESH_ASSET_PATH);
+			M_LOG(TEXT("Default mesh (\"%s\") found, setting it up"), VisibleActorConfig::Default::MESH_ASSET_PATH);
 			Mesh->SetStaticMesh(MeshFinder.Get());
 		}
 
-		Mesh->SetupAttachment(InAttachTo);
+		if(InAttachTo)
+		{
+			Mesh->SetupAttachment(InAttachTo);
+		}
 	}
 }
 
 void ATUPawn::InitProxSphere(USceneComponent* InAttachTo)
 {
-	checkf(InAttachTo, TEXT("When calling %s component to attach to must be non-NULL pointer"), TEXT(__FUNCTION__));
-
 	ProxSphere = CreateDefaultSubobject<USphereComponent>(TEXT("Sphere"));
-	ProxSphere->InitSphereRadius(Default::PROX_SPHERE_RADIUS);
-	ProxSphere->RelativeLocation = Default::MESH_REAL_CENTER_ACTOR_SPACE_LOCATION;
-	ProxSphere->SetupAttachment(InAttachTo);
+	ProxSphere->InitSphereRadius(VisibleActorConfig::Default::PROX_SPHERE_RADIUS);
+	ProxSphere->RelativeLocation = VisibleActorConfig::Default::MESH_REAL_CENTER_ACTOR_SPACE_LOCATION;
+	if(InAttachTo)
+	{
+		ProxSphere->SetupAttachment(InAttachTo);
+	}
 }
 
 TScriptInterface<ITUController> ATUPawn::K2GetTUController() const
