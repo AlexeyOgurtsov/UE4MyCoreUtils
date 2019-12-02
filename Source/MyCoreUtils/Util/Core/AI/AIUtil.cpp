3ee7ccdfb5525a9715f0a88d6dd3af63ccb933d1
alexey_eng <alexey_eng@mail.ru>
2019-12-02 13:25:43 +0500

    Add AI util lib

---
+++ Source/MyCoreUtils/Util/Core/AI/AIUtil.cpp
@@ -0,0 +1,96 @@
+#include "AIUtil.h"
+#include "Util/Core/LogUtilLib.h"
+
+UAIUtil::UAIUtil()
+{
+}
+
+FString UAIUtil::GetPathFollowingResultString(EPathFollowingResult::Type Code)
+{
+	switch(Code)
+	{
+	case EPathFollowingResult::Type::Success:
+		return FString(TEXT("Success"));
+	case EPathFollowingResult::Type::Blocked:
+		return FString(TEXT("Blocked"));
+	case EPathFollowingResult::Type::OffPath:
+		return FString(TEXT("OffPath"));
+	case EPathFollowingResult::Type::Aborted:
+		return FString(TEXT("Aborted"));
+	case EPathFollowingResult::Type::Skipped_DEPRECATED:
+		return FString(TEXT("Skipped_DEPRECATED"));
+	case EPathFollowingResult::Type::Invalid:
+		return FString(TEXT("Invalid"));
+	default:
+		break;
+	}
+	return FString(TEXT("Unknown"));
+}
+
+FString UAIUtil::GetPathFollowingResultFlagsString(FPathFollowingResultFlags::Type const InFlags)
+{
+	FString Result;
+	if((InFlags & FPathFollowingResultFlags::Success) != FPathFollowingResultFlags::None)
+	{
+		Result += FString(TEXT("|Success"));
+	}  
+	if((InFlags & FPathFollowingResultFlags::Blocked) != FPathFollowingResultFlags::None)
+	{
+		Result += FString(TEXT("|Blocked"));
+	}  
+	if((InFlags & FPathFollowingResultFlags::OffPath) != FPathFollowingResultFlags::None)
+	{
+		Result += FString(TEXT("|OffPath"));
+	}  
+	if((InFlags & FPathFollowingResultFlags::UserAbort) != FPathFollowingResultFlags::None)
+	{
+		Result += FString(TEXT("|UserAbort"));
+	}  
+	if((InFlags & FPathFollowingResultFlags::OwnerFinished) != FPathFollowingResultFlags::None)
+	{
+		Result += FString(TEXT("|OwnerFinished"));
+	}  
+	if((InFlags & FPathFollowingResultFlags::InvalidPath) != FPathFollowingResultFlags::None)
+	{
+		Result += FString(TEXT("|InvalidPath"));
+	}  
+	if((InFlags & FPathFollowingResultFlags::MovementStop) != FPathFollowingResultFlags::None)
+	{
+		Result += FString(TEXT("|MovementStop"));
+	}  
+	if((InFlags & FPathFollowingResultFlags::NewRequest) != FPathFollowingResultFlags::None)
+	{
+		Result += FString(TEXT("|NewRequest"));
+	}  
+	if((InFlags & FPathFollowingResultFlags::ForcedScript) != FPathFollowingResultFlags::None)
+	{
+		Result += FString(TEXT("|ForcedScript"));
+	}  
+	if((InFlags & FPathFollowingResultFlags::AlreadyAtGoal) != FPathFollowingResultFlags::None)
+	{
+		Result += FString(TEXT("|AlreadyAtGoal"));
+	}  
+	if((InFlags & FPathFollowingResultFlags::FirstGameplayFlagShift) != FPathFollowingResultFlags::None)
+	{
+		Result += FString(TEXT("|FirstGameplayFlagShift"));
+	}  
+	return Result;
+}
+
+void UAIUtil::LogPathFollowingResultCode(const FString& InKey, EPathFollowingResult::Type const Code)
+{
+	ULogUtilLib::LogString(InKey, GetPathFollowingResultString(Code));
+}
+
+void UAIUtil::LogPathFollowingResult(const FString& InKey, const FPathFollowingResult& Result)
+{
+	M_LOG(TEXT("%s: logging PathFollowingResult"), *InKey);
+	// NOTE: ToString() implementation requires extra module include (AIModule)
+	//M_LOG(TEXT("ToString: %s"), *Result.ToString());
+	LogPathFollowingResultCode(TEXT("Code"), Result.Code);
+	LogPathFollowingResultFlags(TEXT("Flags"), Result.Flags);
+}
+void UAIUtil::LogPathFollowingResultFlags(const FString& InKey, FPathFollowingResultFlags::Type const InFlags)
+{
+	ULogUtilLib::LogString(InKey, GetPathFollowingResultFlagsString(InFlags));
+}
---
+++ Source/MyCoreUtils/Util/Core/AI/AIUtil.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "Navigation/PathFollowingComponent.h"
+#include "AIUtil.generated.h"
+
+UCLASS()
+class UAIUtil : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+	UAIUtil();
+
+	static FString GetPathFollowingResultString(EPathFollowingResult::Type Code);
+
+	static void LogPathFollowingResultCode(const FString& InKey, EPathFollowingResult::Type Code);
+
+	static void LogPathFollowingResult(const FString& InKey, const FPathFollowingResult& Result);
+
+	static void LogPathFollowingResultFlags(const FString& InKey, FPathFollowingResultFlags::Type InFlags);
+
+	static FString GetPathFollowingResultFlagsString(FPathFollowingResultFlags::Type InFlags);
+};
