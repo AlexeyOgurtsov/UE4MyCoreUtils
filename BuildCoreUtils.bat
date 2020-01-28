a57357d7dbc163204b307ef9a7450613ffa76187
alexey_eng <alexey_eng@mail.ru>
2020-01-28 12:07:03 +0500

    Add Build CoreUtils bats

---
+++ BuildCoreUtils.bat
@@ -0,0 +1,7 @@
+D:
+set location=D:\UNR_SCRIPT\LIB
+set projectname=MyCoreUtils
+set projectdirname=MyCoreUtils
+set configpostfix=Editor
+set sniploc=D:\UNR_SCRIPT\SNIPPETS
+call %sniploc%\MyBuild.bat %projectname%%configpostfix% %location%\%projectdirname%\%projectname%
